21b6249a5e77b12d5c97d27e13d82a37723b8549
Aldo Cortesi <aldo@corte.si>
2021-07-21 23:13:47 +1200

    Inner and outer resizing for view

--- canopy/src/geom/view.rs
+++ canopy/src/geom/view.rs
@@ -22,8 +22,11 @@ impl View {
             })
         }
     }
-    pub fn scroll_to(&mut self, x: u16, y: u16) -> Result<()> {
-        Ok(())
+    pub fn scroll_to(&mut self, x: u16, y: u16) {
+        let r = Rect::new(x, y, self.view.w, self.view.h);
+        // We unwrap here, because this can only be an error if view is larger
+        // than outer, which we ensure is not the case.
+        self.view = r.clamp(self.outer).unwrap();
     }
 
     /// Scroll the view by the given offsets. The view rectangle is clamped
@@ -61,11 +64,79 @@ impl View {
     pub fn right(&mut self) {
         self.scroll_by(1, 0);
     }
+
+    /// Return the inner view area.
+    pub fn view(&self) -> Rect {
+        self.view
+    }
+
+    /// Return the enclosing area.
+    pub fn outer(&self) -> Rect {
+        self.outer
+    }
+
+    /// Resize the outer rectangle. The view rectangle is left in place if
+    /// possible, but is shifted if needed. If the view rectangle is larger than
+    /// the new outer size, it is resized to be equal to outer and located at 0,
+    /// 0.
+    pub fn resize_outer(&mut self, outer: Rect) {
+        let view = if outer.w < self.view.w || outer.h < self.view.h {
+            outer
+        } else {
+            self.view
+        };
+
+        // We can unwrap, because this only errors if view is larger than outer,
+        // which can't be the case.
+        self.view = view.clamp(outer).unwrap();
+        self.outer = outer;
+    }
+
+    /// Set the inner rectangle. The inner rectangle is shifted to fit. If the
+    /// inner rectangle is larger than the outer rectangle, an error is
+    /// returned.
+    pub fn set_inner(&mut self, inner: Rect) -> Result<()> {
+        self.view = inner.clamp(self.outer)?;
+        Ok(())
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn view_set_inner() -> Result<()> {
+        let mut v = View::new(Rect::new(0, 0, 100, 100), Rect::new(50, 50, 10, 10))?;
+
+        let err = v.set_inner(Rect::new(0, 0, 190, 190));
+        assert!(err.is_err());
+
+        v.set_inner(Rect::new(110, 110, 20, 20))?;
+        assert_eq!(v.view, Rect::new(80, 80, 20, 20));
+
+        Ok(())
+    }
+
+    #[test]
+    fn view_resize_outer() -> Result<()> {
+        let mut v = View::new(Rect::new(0, 0, 100, 100), Rect::new(50, 50, 10, 10))?;
+
+        v.resize_outer(Rect::new(0, 0, 90, 90));
+        assert_eq!(v.outer, Rect::new(0, 0, 90, 90));
+        assert_eq!(v.view, Rect::new(50, 50, 10, 10));
+
+        v.resize_outer(Rect::new(0, 0, 50, 50));
+        assert_eq!(v.view, Rect::new(40, 40, 10, 10));
+
+        v.resize_outer(Rect::new(0, 0, 50, 50));
+        assert_eq!(v.view, Rect::new(40, 40, 10, 10));
+
+        v.resize_outer(Rect::new(0, 0, 5, 5));
+        assert_eq!(v.view, Rect::new(0, 0, 5, 5));
+
+        Ok(())
+    }
 
     #[test]
     fn view_movement() -> Result<()> {
@@ -82,6 +153,12 @@ mod tests {
 
         v.page_up();
         assert_eq!(v.view, Rect::new(0, 0, 10, 10));
+
+        v.scroll_to(50, 50);
+        assert_eq!(v.view, Rect::new(50, 50, 10, 10));
+
+        v.scroll_to(150, 150);
+        assert_eq!(v.view, Rect::new(90, 90, 10, 10));
 
         Ok(())
     }
