f8fc7816f99c3f1b53d713e10ed1f3b05f752603
Aldo Cortesi <aldo@corte.si>
2023-02-22 13:32:24 +1300

    editor: split state into own module, revamp insert implementation

--- canopy/src/widgets/editor/effect.rs
+++ canopy/src/widgets/editor/effect.rs
@@ -1,9 +1,11 @@
-use super::core;
+use super::state;
 
 trait Effector {
-    /// Modifies the provided state in-place, and returns an optional undo
-    /// operation.
-    fn apply(&self, c: core::State) -> core::State;
+    /// Modifies the provided state and returns a new state to apply this effect.
+    fn apply(&self, c: state::State) -> state::State;
+
+    /// Modifies the provided state and returns a new state to revert this effect.
+    fn revert(&self, c: state::State) -> state::State;
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -12,27 +14,80 @@ pub enum Effect {
     Delete(Delete),
 }
 
+impl Effector for Effect {
+    fn apply(&self, s: state::State) -> state::State {
+        match self {
+            Effect::Insert(i) => i.apply(s),
+            Effect::Delete(d) => d.apply(s),
+        }
+    }
+
+    fn revert(&self, c: state::State) -> state::State {
+        match self {
+            Effect::Insert(i) => i.revert(c),
+            Effect::Delete(d) => d.revert(c),
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Insert {
-    pub(crate) pos: core::Position,
-    pub(crate) text: String,
+    pos: state::Position,
+    text: Vec<String>,
+    prev_cursor: state::Position,
+}
+
+impl Insert {
+    pub(super) fn new(s: &state::State, pos: state::Position, text: String) -> Self {
+        Self {
+            pos,
+            text: text.split("\n").map(|s| s.to_string()).collect(),
+            prev_cursor: s.cursor,
+        }
+    }
 }
 
 impl Effector for Insert {
-    fn apply(&self, s: core::State) -> core::State {
-        s.insert(self.pos, &self.text)
+    fn apply(&self, s: state::State) -> state::State {
+        s.insert_lines(self.pos, &self.text)
+    }
+
+    fn revert(&self, s: state::State) -> state::State {
+        s.delete(
+            self.pos,
+            state::Position {
+                line: self.pos.line + self.text.len(),
+                column: self.pos.column,
+            },
+        )
     }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct Delete {
-    start: core::Position,
-    end: core::Position,
+    start: state::Position,
+    end: state::Position,
+    prev_cursor: state::Position,
+}
+
+impl Delete {
+    pub(super) fn new(s: &state::State, start: state::Position, end: state::Position) -> Self {
+        Self {
+            start,
+            end,
+            prev_cursor: s.cursor,
+        }
+    }
 }
 
 impl Effector for Delete {
-    fn apply(&self, s: core::State) -> core::State {
+    fn apply(&self, s: state::State) -> state::State {
         s.delete(self.start, self.end)
+    }
+
+    fn revert(&self, s: state::State) -> state::State {
+        s
+        // s.delete(self.pos, self.pos + self.text.len())
     }
 }
 
--- canopy/src/widgets/editor/mod.rs
+++ canopy/src/widgets/editor/mod.rs
@@ -1,2 +1,3 @@
 pub mod core;
 mod effect;
+mod state;
