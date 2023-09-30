c92c54795d11b1e654e58eb71602ada804b08c6b
cortesi <aldo@corte.si>
2023-09-30 03:06:05 +0000

    Deploying to gh-pages from @ cortesi/canopy@2f6fa55c40832d78cf76d8ff31bf4ef0fca9312e 🚀

--- doc/implementors/core/convert/trait.TryFrom.js
+++ doc/implementors/core/convert/trait.TryFrom.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"canopy":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.72.0/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"canopy/struct.NodeName.html\" title=\"struct canopy::NodeName\">NodeName</a>"]]
+"canopy":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.72.1/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.72.1/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"canopy/struct.NodeName.html\" title=\"struct canopy::NodeName\">NodeName</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
