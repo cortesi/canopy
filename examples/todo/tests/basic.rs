34547430c92560b3d57734ae1d9b14ec557456bf
Aldo Cortesi <aldo@corte.si>
2025-06-27 00:24:40 +1200

    Consolidate todo tests

--- examples/todo/tests/basic.rs
+++ examples/todo/tests/basic.rs
@@ -1,6 +1,5 @@
 use anyhow::Result;
-use canopy::tutils::run_root;
-use canopy::tutils::run_root_with_size;
+use canopy::tutils::{run_root, run_root_with_size, spawn_workspace_bin};
 use std::time::Duration;
 use todo::{bind_keys, open_store, style, Todo};
 
@@ -81,4 +80,26 @@ fn add_item_with_char_newline() {
         Ok(())
     })
     .unwrap();
+}
+
+#[test]
+fn add_item_via_pty() {
+    let db_path = std::env::temp_dir().join(format!(
+        "todo_test_pty_{}.db",
+        std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_millis()
+    ));
+    open_store(db_path.to_str().unwrap()).unwrap();
+
+    let mut app = spawn_workspace_bin("todo", &[db_path.to_str().unwrap()]).unwrap();
+    app.expect("todo", Duration::from_millis(100)).ok();
+
+    app.send("a").unwrap();
+    app.send("hi").unwrap();
+    app.send("\r").unwrap();
+    app.send("q").unwrap();
+
+    app.wait_eof(Duration::from_secs(2)).unwrap();
 }
