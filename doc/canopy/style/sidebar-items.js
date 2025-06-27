821a73c6a0046a43bb33d33ff8e0f76d530a17eb
cortesi <aldo@corte.si>
2025-06-27 10:02:48 +0000

    Deploying to gh-pages from @ cortesi/canopy@54f824c69ab2e7469edb04811368dbf063965be6 🚀

--- doc/canopy/style/sidebar-items.js
+++ doc/canopy/style/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Attr","Color"],"mod":["solarized"],"struct":["AttrSet","Style","StyleManager","StyleMap"]};
+window.SIDEBAR_ITEMS = {"enum":["Attr","Color"],"mod":["solarized"],"struct":["AttrSet","PartialStyle","Style","StyleManager","StyleMap"]};
