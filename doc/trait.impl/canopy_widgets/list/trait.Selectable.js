397f1fb446d43a54829ee285847a0b6fe3747868
cortesi <aldo@corte.si>
2026-01-19 00:45:45 +0000

    Deploying to gh-pages from @ cortesi/canopy@9aa08f33b6a434a4a5b8baa15678f75246c215f2 🚀

--- doc/trait.impl/canopy_widgets/list/trait.Selectable.js
+++ doc/trait.impl/canopy_widgets/list/trait.Selectable.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["canopy_examples",[["impl <a class=\"trait\" href=\"canopy_widgets/list/trait.Selectable.html\" title=\"trait canopy_widgets::list::Selectable\">Selectable</a> for <a class=\"struct\" href=\"canopy_examples/intervals/struct.CounterItem.html\" title=\"struct canopy_examples::intervals::CounterItem\">CounterItem</a>"],["impl <a class=\"trait\" href=\"canopy_widgets/list/trait.Selectable.html\" title=\"trait canopy_widgets::list::Selectable\">Selectable</a> for <a class=\"struct\" href=\"canopy_examples/listgym/struct.ListEntry.html\" title=\"struct canopy_examples::listgym::ListEntry\">ListEntry</a>"]]],["canopy_widgets",[]],["todo",[["impl <a class=\"trait\" href=\"canopy_widgets/list/trait.Selectable.html\" title=\"trait canopy_widgets::list::Selectable\">Selectable</a> for <a class=\"struct\" href=\"todo/struct.TodoEntry.html\" title=\"struct todo::TodoEntry\">TodoEntry</a>"]]]]);
+    var implementors = Object.fromEntries([["canopy_examples",[["impl <a class=\"trait\" href=\"canopy_widgets/list/trait.Selectable.html\" title=\"trait canopy_widgets::list::Selectable\">Selectable</a> for <a class=\"struct\" href=\"canopy_examples/intervals/struct.CounterItem.html\" title=\"struct canopy_examples::intervals::CounterItem\">CounterItem</a>"],["impl <a class=\"trait\" href=\"canopy_widgets/list/trait.Selectable.html\" title=\"trait canopy_widgets::list::Selectable\">Selectable</a> for <a class=\"struct\" href=\"canopy_examples/listgym/struct.ListEntry.html\" title=\"struct canopy_examples::listgym::ListEntry\">ListEntry</a>"]]],["canopy_widgets",[]],["todo",[["impl Selectable for <a class=\"struct\" href=\"todo/struct.TodoEntry.html\" title=\"struct todo::TodoEntry\">TodoEntry</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[609,22,261]}
+//{"start":57,"fragment_lengths":[609,22,138]}
